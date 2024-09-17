3b81b477aaef81c98ba881142eb595e4bfe60c55
ToanNVF8 <ToanNVHE163111@gmail.com>
2024-09-17 23:41:43 +0700

    u

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import BookingDetails from "./screens/BookingDetails";
+import HostProfile from "./screens/HostProfile";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <HostProfile></HostProfile>;
 }
 
 export default App;
