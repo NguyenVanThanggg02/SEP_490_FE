004093593b8a5e53e0da95b83a9672af1b2ecab6
tuanddhe151088 <tuanddhe151088@fpt.edu.vn>
2024-10-24 00:48:26 +0700

    search map

--- src/screens/AddSpaces/AddSpaceLocation.jsx
+++ src/screens/AddSpaces/AddSpaceLocation.jsx
@@ -1,34 +1,47 @@
-import React, { useContext } from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import { TextField, Typography } from '@mui/material';
-import { SpaceContext } from '../../Context/SpaceContext ';
+import { MapSearch } from '../../components/Map';
+import { Select } from 'antd';
 
 const AddSpaceLocation = () => {
-    const { location, setLocation } = useContext(SpaceContext);
+    const [location, setLocation] = useState('');
+    const [location2, setLocation2] = useState(''); // trường hợp click kéo thả marker 
 
-    const handleAddressChange = (e) => {
-        setLocation(e.target.value );
-    };
+    const [address, setAddress] = useState('');
+    const [locationSuggest, setLocationSuggest] = useState(null);
+console.log("locationSuggest", locationSuggest);
 
 
     return (
         <Container fluid>
             <Row className="pb-5">
-                <Typography variant='h4' fontWeight={700} className="text-center" >Vị trí không gian của bạn</Typography>
+                <Typography variant='h4' fontWeight={700} className="text-center">Vị trí không gian của bạn</Typography>
             </Row>
             <Row className="d-flex justify-content-center align-items-center">
                 <Col md={6}>
                     <Row>
-                    <Typography variant="h6"  style={{ fontWeight: 700,fontSize:"20px" }} >Nhập địa chỉ <span style={{color:"red"}}>*</span></Typography>
-                        <TextField
-                            variant="outlined"
-                            value={location}
-                            onChange={handleAddressChange}
-                            fullWidth
+                        <Typography variant="h6" style={{ fontWeight: 700, fontSize: "20px" }}>Nhập địa chỉ <span style={{ color: "red" }}>*</span></Typography>
+                        
+                        <Select
+                        size='large'
+                        style={{marginBottom: 50}}
+                            onInputKeyDown={(e) => {
+                                console.log("nhập", e.target.value); setAddress(e.target.value)
+                            }}
+                            showSearch
+                            placeholder="Nhập địa chỉ"
+                            filterOption={(input, option) =>
+                                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+                            }
+                            options={locationSuggest}
+                            onChange={(e) => setLocation(e)}
+                            value={location || location2}
                         />
                     </Row>
                 </Col>
             </Row>
+            <MapSearch textSearch={address} setLocationSuggest={setLocationSuggest} location={location} setLocation={setLocation} setLocation2={setLocation2}/>
         </Container>
     );
 };
