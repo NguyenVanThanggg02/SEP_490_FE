1bf71e90f4380bba1b61c754b8f9c46bc6e03d49
NKPToan27 <nguyenkhacphongt@gmail.com>
2024-10-24 01:44:23 +0700

    ui mana space

--- src/screens/ManaSpaceHost/SpacePosted.jsx
+++ src/screens/ManaSpaceHost/SpacePosted.jsx
@@ -3,8 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/Add';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
 import axios from 'axios';
 import { Pane, Spinner } from "evergreen-ui";
+import { Link } from "react-router-dom";
 
 const SpacePosted = () => {
     const [listPosted, setlistPosted] = useState([]);
@@ -40,6 +44,29 @@ const SpacePosted = () => {
             </Pane>
         );
     }
+    const renderCensorshipIcon = (censorship) => {
+        switch (censorship) {
+
+            case 'Chờ duyệt':
+                return <Typography variant="body2" sx={{ color: '#FFCA28' }}>
+                    <AccessTimeIcon /> Chờ duyệt
+                </Typography>;
+            case 'Chấp nhận':
+                return <Typography variant="body2" sx={{ color: '#4CAF50' }}>
+                    <CheckIcon /> Chấp nhận
+                </Typography>;
+            case 'Từ chối':
+                return <Typography variant="body2" sx={{ color: '#F44336' }}>
+                    <CloseIcon /> Từ chối
+                </Typography>;
+            default:
+                return null;
+        }
+    };
+
+
+
+
     return (
         <Container>
             <Row className="pb-5">
@@ -47,7 +74,7 @@ const SpacePosted = () => {
                     Danh sách phòng cho thuê
                 </Typography>
             </Row>
-            <Row>
+            <Row className="pb-5">
                 <Col md={5} style={{ marginLeft: "auto" }}>
                     <TextField id="outlined-basic" placeholder='Tên không gian' variant="outlined" size="small"
 
@@ -64,31 +91,37 @@ const SpacePosted = () => {
                     </Typography>
                 ) : (
                     listPosted.map((lpost) => (
-                        <Col md={4}>
-
-                            <Card key={lpost.id} sx={{ maxWidth: 345 }}>
+                        <Col md={3} className='pb-5'>
+                            <Card key={lpost.id} sx={{ maxWidth: 345, height: "100%" }}>
                                 <CardMedia
-                                    sx={{ height: 140 }}
+                                    sx={{ height: 200 }}
                                     image={lpost.images[0]?.url || "path/to/default/image.jpg"} // Thay thế với đường dẫn hình ảnh mặc định nếu không có
                                     title={lpost.name}
                                 />
                                 <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
+                                    <Typography gutterBottom variant="h6" component="div" noWrap>
                                         {lpost.name}
                                     </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
                                         {lpost.location}
                                     </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                                    <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
                                         {lpost.pricePerHour} VNĐ/giờ
                                     </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                        {lpost.userId?.name} {/* Đảm bảo rằng userId tồn tại trước khi truy cập name */}
+                                    <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+                                        Host {lpost.userId?.username}
                                     </Typography>
                                 </CardContent>
                                 <CardActions>
-                                    <Button size="small">Share</Button>
-                                    <Button size="small">Learn More</Button>
+                                    <Button size="small" variant='contained' disableElevation   sx={{ textTransform: 'none' }}
+                                    >Chỉnh sửa</Button>
+                                    <Link
+                                        to={`/spaces/${lpost._id}`}
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        <Button size="small" variant='outlined' sx={{ textTransform: 'none' }}>Xem phòng</Button>
+                                    </Link>
+                                    {renderCensorshipIcon(lpost.censorship)}
                                 </CardActions>
                             </Card>
                         </Col>
