7aa124d0986718e7484c93bb0d7d0f77b4bb9631
kun_dev <cong.nguyen@vangxanh.net>
2024-10-26 13:01:42 +0700

    update get space noi bat

--- src/screens/SpaceDetails.jsx
+++ src/screens/SpaceDetails.jsx
@@ -128,9 +128,8 @@ function SpaceDetails() {
         Error loading data.
       </Typography>
     );
-  const mainImage = spaceData?.images[0]?.url;
-  const otherImages = spaceData?.images.slice(1, 5).map(image => image.url);
-
+  const mainImage = spaceData?.images?.[0]?.url;
+  const otherImages = spaceData?.images ? spaceData.images.slice(1, 5).map(image => image.url) : [];
 
   const appliances = spaceData?.appliancesId || [];
   const images = spaceData?.images || [];
@@ -167,7 +166,7 @@ function SpaceDetails() {
         <Card style={{ position: "relative" }}>
           <CardMedia
             sx={{ height: 250 }}
-            image={spaceData?.images[0].url || "default-image"}
+            image={spaceData?.images?.[0]?.url || "default-image"}
             title="image spaceF"
             style={{ objectFit: "cover" }}
           />
