ba5dcab9e6e87dc1e5f483b386599720c850c824
Hoà Hữu <anhviphuong456@gmail.com>
2024-11-05 21:33:52 +0700

    updatecancel

--- src/screens/CancellationPolicy.jsx
+++ src/screens/CancellationPolicy.jsx
@@ -7,7 +7,7 @@ const CancellationPolicy = () => {
       <div className="header">Chính sách hủy</div>
       <div className="subheader">
         Đảm bảo chính sách của Chủ nhà/Người tổ chức này phù hợp với bạn. Trong một số trường hợp hiếm hoi, bạn có thể đủ điều kiện được hoàn tiền ngoài chính sách này, theo{" "}
-        <a href="#">Chính sách sự kiện lớn gây gián đoạn của Airbnb</a>.
+        <a href="#">Chính sách sự kiện lớn gây gián đoạn</a>.
       </div>
 
       <div className="policy-section">
@@ -29,6 +29,18 @@ const CancellationPolicy = () => {
         <div className="row">
           <div className="col-4">14 thg 1 14:00</div>
           <div className="col-8">Nhận tiền hoàn lại cho tất cả các đêm, ngoại trừ đêm đầu tiên. Không được hoàn lại phí dịch vụ hoặc chi phí cho đêm đầu tiên.</div>
+        </div>
+      </div>
+
+      {/* Thêm chính sách hủy bổ sung */}
+      <div className="policy-section">
+        <div className="row">
+          <div className="col-4">Trong vòng 24 giờ</div>
+          <div className="col-8">Không được hoàn tiền</div>
+        </div>
+        <div className="row">
+          <div className="col-4">15 thg 1 14:00</div>
+          <div className="col-8">Không được hoàn tiền cho bất kỳ đêm nào nếu hủy trong vòng 24 giờ trước thời gian nhận phòng.</div>
         </div>
       </div>
 
