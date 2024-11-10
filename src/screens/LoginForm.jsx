c20b21019f074dca9befb6abba9c58e70b8d9660
Datnd <nguyenducdat0610@gmail.com>
2024-11-10 22:38:40 +0700

    updatelelogin

--- src/screens/LoginForm.jsx
+++ src/screens/LoginForm.jsx
@@ -9,30 +9,23 @@ import "react-toastify/dist/ReactToastify.css";
 
 const LoginForm = ({ setIsLoggedIn }) => {
   const formRef = useRef(null);
-  const nav = useNavigate();
+  const navigate = useNavigate();
   const [error, setError] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const form = formRef.current;
-    const username = form.elements["email_field"].value; // Sử dụng id đúng
-    const password = form.elements["password_field"].value; // Sử dụng id đúng
-    const data = { username, password };
+
+    const formData = {
+      username: formRef.current.elements["email_field"].value,
+      password: formRef.current.elements["password_field"].value,
+    };
+
     try {
-      const res = await axios.post("http://localhost:9999/users/login", data, {
-        headers: {
-          "Content-Type": "application/json",
-        },
+      const response = await axios.post("http://localhost:9999/users/login", formData, {
+        headers: { "Content-Type": "application/json" },
       });
-      const {
-        accessToken,
-        refreshToken,
-        username,
-        id,
-        fullname,
-        role,
-        firstLogin,
-      } = res.data;
+
+      const { accessToken, refreshToken, username, id, fullname, role, firstLogin } = response.data;
       localStorage.setItem("refreshToken", refreshToken);
       localStorage.setItem("accessToken", accessToken);
       localStorage.setItem("username", username);
@@ -41,112 +34,83 @@ const LoginForm = ({ setIsLoggedIn }) => {
       localStorage.setItem("role", role);
       localStorage.setItem("firstLogin", firstLogin);
       localStorage.setItem("isLoggedIn", "true");
+
       toast.success("Đăng nhập thành công!");
       setIsLoggedIn(true);
 
       if (role === 1) {
-        nav("/admin");
+        navigate("/admin");
       } else {
-        if (firstLogin === true) {
-          nav("/userneed");
-          return;
-        }
-        nav("/");
+        navigate(firstLogin ? "/userneed" : "/");
       }
     } catch (error) {
-      if (error.response) {
-        if (error.response.status === 403) {
-          toast.error("Người dùng bị cấm!");
-        } else {
-          setError(error.response.data.error || "Đăng nhập không thành công!");
-          toast.error(
-            error.response.data.error ||
-              "Tên người dùng hoặc mật khẩu không chính xác!"
-          );
-        }
-      } else {
-        toast.error("Đã xảy ra lỗi không xác định!");
-      }
+      handleLoginError(error);
     }
   };
+
+  const handleLoginError = (error) => {
+    if (error.response) {
+      const errorMessage = error.response.data.error || "Đăng nhập không thành công!";
+      if (error.response.status === 403) {
+        toast.error("Người dùng bị cấm!");
+      } else {
+        setError(errorMessage);
+        toast.error(errorMessage);
+      }
+    } else {
+      toast.error("Đã xảy ra lỗi không xác định!");
+    }
+  };
+
   return (
     <Container>
-      <Row
-        className="justify-content-center align-items-center"
-        // style={{ height: "100vh" }}
-      >
+      <Row className="justify-content-center align-items-center">
         <Col xs={12} md={6}>
-          <div>
-            <form
-              className="form_container"
-              onSubmit={handleSubmit}
-              ref={formRef}
-            >
-              <div
-                className="logo_container d-flex justify-content-center"
-                style={{ width: "100%" }}
-              >
-                <img
-                  src={logo}
-                  height={80}
-                  alt="logo"
-                  style={{ objectFit: "contain" }}
-                />
-              </div>
-              <div className="title_container">
-                <p className="title">Đăng nhập với tài khoản của bạn</p>
-                <span className="subtitle">
-                  Bắt đầu với ứng dụng của chúng tôi, chỉ cần tạo một tài khoản
-                  và tận hưởng trải nghiệm.
-                </span>
-              </div>
-              <br />
-              <div className="input_container">
-                <label className="input_label" htmlFor="email_field">
-                  Tên đăng nhập
-                </label>
-                <input
-                  placeholder="Enter your username"
-                  name="input-name"
-                  type="text"
-                  className="input_field"
-                  id="email_field"
-                />
-              </div>
-              <div className="input_container">
-                <label className="input_label" htmlFor="password_field">
-                  Mật khẩu
-                </label>
-                <input
-                  placeholder="Password"
-                  name="password_field"
-                  type="password"
-                  className="input_field"
-                  id="password_field"
-                />
-              </div>
-              <Button
-                variant="dark"
-                className="sign-in_btn mb-3 mt-2"
-                type="submit"
-              >
-                Đăng nhập
-              </Button>
-              <div>
-                <Link to={"/register"} className="text-danger text-opacity-75">
-                  Tôi chưa có tài khoản ...
-                </Link>
-              </div>{" "}
-              <div>
-                <Link to={"/forgot"} className="text-danger text-opacity-75">
-                  Tôi quên mật khẩu
-                </Link>
-              </div>
-            </form>
-          </div>
+          <form className="form_container" onSubmit={handleSubmit} ref={formRef}>
+            <div className="logo_container d-flex justify-content-center">
+              <img src={logo} height={80} alt="logo" style={{ objectFit: "contain" }} />
+            </div>
+            <div className="title_container">
+              <p className="title">Đăng nhập với tài khoản của bạn</p>
+              <span className="subtitle">
+                Bắt đầu với ứng dụng của chúng tôi, chỉ cần tạo một tài khoản và tận hưởng trải nghiệm.
+              </span>
+            </div>
+
+            <div className="input_container">
+              <label htmlFor="email_field" className="input_label">Tên đăng nhập</label>
+              <input
+                id="email_field"
+                type="text"
+                placeholder="Enter your username"
+                className="input_field"
+              />
+            </div>
+
+            <div className="input_container">
+              <label htmlFor="password_field" className="input_label">Mật khẩu</label>
+              <input
+                id="password_field"
+                type="password"
+                placeholder="Password"
+                className="input_field"
+              />
+            </div>
+
+            <Button variant="dark" className="sign-in_btn mb-3 mt-2" type="submit">
+              Đăng nhập
+            </Button>
+
+            <div>
+              <Link to="/register" className="text-danger text-opacity-75">Tôi chưa có tài khoản ...</Link>
+            </div>
+            <div>
+              <Link to="/forgot" className="text-danger text-opacity-75">Tôi quên mật khẩu</Link>
+            </div>
+          </form>
         </Col>
       </Row>
-      <ToastContainer /> {/* Tự đóng toast sau 3 giây */}
+      <ToastContainer autoClose={3000} />
     </Container>
   );
 };
