293dab025996dfd466d3b2ca5c16d200c4529000
Hoà Hữu <anhviphuong456@gmail.com>
2024-11-11 22:17:57 +0700

    manaacountcss

--- src/screens/accountmanager.jsx
+++ src/screens/accountmanager.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import './accountmanager.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard, faShieldAlt, faWallet, faFileAlt, faBell, faEye, faSlidersH, faBriefcase, faChartBar } from '@fortawesome/free-solid-svg-icons';
+import "../style/accountmanager.css";
 
 const AccountManager = () => {
   return (
