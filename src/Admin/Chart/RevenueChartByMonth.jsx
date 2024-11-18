11ca65ed15c5d3d2807b509d6ded43cec2c75a67
Datnd <nguyenducdat0610@gmail.com>
2024-11-18 22:28:43 +0700

    revenueChart

--- src/Admin/Chart/RevenueChartByMonth.jsx
+++ src/Admin/Chart/RevenueChartByMonth.jsx
@@ -1,21 +1,27 @@
-import * as React from "react";
+import React from "react";
 import { LineChart } from "@mui/x-charts/LineChart";
 import { Container } from "react-bootstrap";
 
-export default function RevenueChart() {
+const RevenueChart = () => {
+  const chartData = {
+    xAxisData: [1, 2, 3, 5, 8, 10],
+    seriesData: [2, 5.5, 2, 8.5, 1.5, 5],
+  };
+
   return (
     <Container>
       <LineChart
-        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        xAxis={[{ data: chartData.xAxisData }]}
         series={[
           {
-            data: [2, 5.5, 2, 8.5, 1.5, 5],
+            data: chartData.seriesData,
           },
         ]}
         width={500}
         height={300}
       />
-    
     </Container>
   );
-}
+};
+
+export default RevenueChart;
