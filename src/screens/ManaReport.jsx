d6ca030177efa81da59825d0908a6c5c42bfe3a0
NKPToan27 <nguyenkhacphongt@gmail.com>
2024-12-16 01:46:00 +0700

    form mana report for space owner

--- src/screens/ManaReport.jsx
+++ src/screens/ManaReport.jsx
@@ -11,8 +11,12 @@ import { Pane, Spinner } from "evergreen-ui";
 import { Link } from "react-router-dom";
 import { ExclamationCircleFill } from 'react-bootstrap-icons';
 import { Paginator } from 'primereact/paginator';
+import { toast } from 'react-toastify';
 const ManaReport = () => {
     const [spaceReported, setspaceReported] = useState([]);
+    const [complaint, setComplaint] = useState("");
+    const [dialogComplaint,setDialogComplaint ] = useState(false);
+    const [selectedReportId, setSelectedReportId] = useState(null); // Report ID lưu tạm
     const [loading, setLoading] = useState(true);
     const [openDialog, setOpenDialog] = useState(false);
     const [selectedSpace, setSelectedSpace] = useState(null);
@@ -23,7 +27,7 @@ const ManaReport = () => {
     const userId = localStorage.getItem('userId');
     useEffect(() => {
         axios
-            .get("http://localhost:9999/reports")
+        .get(`http://localhost:9999/reports/${userId}`)
             .then((response) => {
                 const sortedSpaces = response.data.sort(
                     (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
@@ -37,6 +41,31 @@ const ManaReport = () => {
                 setLoading(false);
             });
     }, []);
+
+    const openComplaintDialog = (reportId) => {
+        setSelectedReportId(reportId); // Lưu ID báo cáo hiện tại
+        setComplaint(""); // Reset input
+        setDialogComplaint(true); // Hiển thị dialog
+      };
+    
+    const updateComplaint = async () => {
+        if (!complaint.trim()) {
+            toast.error("Lý do khiếu nại không được để trống.");
+            return;
+          }
+        try {
+          const response = await axios.put(
+            `http://localhost:9999/reports/complaint/${selectedReportId}`,
+            { complaint: complaint.trim()}
+          );
+            toast.success("Gửi đơn khiếu nại thành công!");
+            setDialogComplaint(false); // Đóng dialog
+            setSelectedReportId(null); // Reset reportId
+
+        } catch (error) {
+          console.error("Lỗi khi cập nhật complaint:", error.response?.data);
+        }
+      };
     const onPageChange = async (event) => {
         setFirst(event?.first);
         setCurrentPage(event.page + 1);
@@ -54,32 +83,8 @@ const ManaReport = () => {
             </Pane>
         );
     }
-    const renderCensorshipIcon = (censorship) => {
-        switch (censorship) {
-            case 'Chờ duyệt':
-                return <Typography variant="body2" sx={{ color: '#FFCA28' }}>
-                    <AccessTimeIcon /> Chờ duyệt
-                </Typography>;
-            case 'Chấp nhận':
-                return <Typography variant="body2" sx={{ color: '#4CAF50' }}>
-                    <CheckIcon /> Chấp nhận
-                </Typography>;
-            case 'Từ chối':
-                return <Typography variant="body2" sx={{ color: '#F44336' }}>
-                    <CloseIcon /> Từ chối
-                </Typography>;
-            default:
-                return null;
-        }
-    };
-    const handleCloseDialog = () => {
-        setOpenDialog(false);
-        setSelectedSpace(null);
-    };
-    const currentReportCount = spaceReported?.spaceId?.reportCount || 0;
-    const remainingReports = 3 - currentReportCount
     return (
-        <Container>
+        <Container fluid>
             <Row className="pb-5">
                 <Typography variant="h4" className="text-center">
                     Danh sách không gian bị tố cáo
@@ -104,67 +109,76 @@ const ManaReport = () => {
                 </Col>
             </Row>
             <Row>
-        {listPostedOnPage.length === 0 ? (
-            <Typography variant="body1" align="center">
-                Không có tố cáo.
-            </Typography>
-        ) : (
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={{ fontWeight: 700 }}>STT</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Tên</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Ảnh</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Người tố cáo</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Lý do tố cáo</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Lượt tố cáo thành công</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Trạng thái không gian</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Trạng thái báo cáo</TableCell>
-                        <TableCell sx={{ fontWeight: 700 }}>Thao tác</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {listPostedOnPage.map((lpost, index) => (
-                        <TableRow key={lpost._id}>
-                            <TableCell>{index + 1}</TableCell>
-                            <TableCell>{lpost?.spaceId?.name}</TableCell>
-                            <TableCell>
-                                <img 
-                                    src={lpost?.spaceId?.images?.[0]?.url || "path/to/default/image.jpg"} 
-                                    alt={lpost?.spaceId?.name} 
-                                    style={{ width: "100px", height: "auto" }} 
-                                />
-                            </TableCell>
-                            <TableCell>{lpost?.userId?.fullname}</TableCell>
-                            <TableCell>
-                            {lpost.reasonId.map((reason) => reason.text.join(", ")).join("; ")}
-                            {lpost.customReason && `; ${lpost.customReason}`}
-                            </TableCell>
-                            <TableCell>{lpost?.spaceId?.reportCount}</TableCell>
-                            <TableCell sx={{ color: lpost?.spaceId?.censorship === 'Chấp nhận' ? 'green' : 'red' }}>
-                                {lpost?.spaceId?.censorship}
-                            </TableCell>
-                            <TableCell sx={{ color: lpost.statusReport === 'Từ chối' ? 'error.main' : lpost.statusReport === 'Chấp nhận' ? 'success.main' : 'warning.main' }}>
-                                {lpost.statusReport}
-                            </TableCell>
-                            <TableCell>
-                                <div style={{ display: "flex", gap: "8px" }}>
-                                    <Link to={`/spaces/${lpost?.spaceId?._id}`} style={{ textDecoration: "none" }}>
-                                        <Button size="small" variant="outlined" sx={{ textTransform: "none" }}>
-                                            Xem phòng
-                                        </Button>
-                                    </Link>
-                                    <Button size="small" variant="contained" color="info" sx={{ textTransform: "none" }}>
-                                        Khiếu nại
-                                    </Button>
-                                </div>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        )}
-    </Row>
+            {listPostedOnPage.length === 0 ? (
+                    <Typography variant="body1" align="center">
+                        Không có tố cáo.
+                    </Typography>
+                ) : (
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell sx={{ fontWeight: 700 }}>STT</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Tên</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Ảnh</TableCell>
+                                <TableCell sx={{ fontWeight: 700, width: '180px' }}>Người tố cáo</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Lý do tố cáo</TableCell>
+                                <TableCell sx={{ fontWeight: 700, width: '80px' }}>Lượt tố cáo thành công</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Trạng thái không gian</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Trạng thái báo cáo</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Trạng thái khiếu nại</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Nội dung đã khiếu nại</TableCell>
+                                <TableCell sx={{ fontWeight: 700 }}>Thao tác</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {listPostedOnPage.map((lpost, index) => (
+                                <TableRow key={lpost._id}>
+                                    <TableCell>{index + 1}</TableCell>
+                                    <TableCell>{lpost?.spaceId?.name}</TableCell>
+                                    <TableCell>
+                                        <img
+                                            src={lpost?.spaceId?.images?.[0]?.url || "path/to/default/image.jpg"}
+                                            alt={lpost?.spaceId?.name}
+                                            style={{ width: "100px", height: "auto" }}
+                                        />
+                                    </TableCell>
+                                    <TableCell>{lpost?.userId?.fullname}</TableCell>
+                                    <TableCell>
+                                        {lpost.reasonId.map((reason) => reason.text.join(", ")).join("; ")}
+                                        {lpost.customReason && `; ${lpost.customReason}`}
+                                    </TableCell>
+                                    <TableCell>{lpost?.spaceId?.reportCount}</TableCell>
+                                    <TableCell sx={{ color: lpost?.spaceId?.censorship === 'Chấp nhận' ? 'green' : 'red' }}>
+                                        {lpost?.spaceId?.censorship}
+                                    </TableCell>
+                                    <TableCell sx={{ color: lpost.statusReport === 'Từ chối' ? 'error.main' : lpost.statusReport === 'Chấp nhận' ? 'success.main' : 'warning.main' }}>
+                                        {lpost.statusReport}
+                                    </TableCell>
+                                    <TableCell sx={{ color: lpost.statusComplaint === 'Từ chối' ? 'error.main' : lpost.statusComplaint === 'Chấp nhận' ? 'success.main' : 'warning.main' }}>
+                                        {lpost.statusComplaint}
+                                    </TableCell>
+                                    <TableCell>
+                                        {lpost.complaint}
+                                    </TableCell>
+                                    <TableCell>
+                                        <div style={{ display: "flex", gap: "8px" }}>
+                                            <Link to={`/spaces/${lpost?.spaceId?._id}`} style={{ textDecoration: "none" }}>
+                                                <Button size="small" variant="outlined" sx={{ textTransform: "none" }}>
+                                                    Xem phòng
+                                                </Button>
+                                            </Link>
+                                            <Button size="small" variant="contained" color="info" sx={{ textTransform: "none" }} 
+                                            onClick={()=>openComplaintDialog(lpost._id)}>
+                                                Khiếu nại
+                                            </Button>
+                                        </div>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                )}
+            </Row>
             <Row
                 style={{
                     display: "flex",
