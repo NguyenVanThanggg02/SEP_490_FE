20778312d01edc19fcc6d27efce6ef05479a4209
kun_dev <thang2k210@gmail.com>
2024-12-16 11:30:20 +0700

    update package

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@radix-ui/themes/styles.css";
+// import "@radix-ui/themes/styles.css";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { SpaceProvider } from "./Context/SpaceContext ";
